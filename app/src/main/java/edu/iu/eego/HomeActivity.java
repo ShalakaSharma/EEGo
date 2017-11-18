3fc22999599ba1bef05e719f730e33ce90d32f69
Shalaka Sharma <sharmask@iu.edu>
2017-11-18 14:21:45 -0500

    Removed gitignore

--- app/src/main/java/edu/iu/eego/HomeActivity.java
+++ app/src/main/java/edu/iu/eego/HomeActivity.java
@@ -1,7 +1,15 @@
 package edu.iu.eego;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +17,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button btn = (Button) findViewById(R.id.button);
     }
+
+    public void onClickNextButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), ConnectionActivity.class);
+        startActivity(intent);
+
+    }
+
 }
